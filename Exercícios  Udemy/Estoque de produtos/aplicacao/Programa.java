0a80fcf64bc420310d201373c7dd76980a88a353
Beatriz Alves <bearizt3@gmail.com>
2022-10-26 23:46:25 -0300

    Utilizando construtor

--- Exercícios  Udemy/Estoque de produtos/aplicacao/Programa.java
+++ Exercícios  Udemy/Estoque de produtos/aplicacao/Programa.java
@@ -8,22 +8,23 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
-		Produto produto = new Produto();
+				
 		System.out.println("Insira os dados do produto: ");
 		System.out.println("Nome: ");
-		produto.nome = sc.nextLine();
+		String nome = sc.nextLine();
 		System.out.println("Preço: ");
-		produto.preco = sc.nextDouble();
+		double preco = sc.nextDouble();
 		System.out.println("Quantidade em estoque: ");
-		produto.quantidade = sc.nextInt();
+		int quantidade = sc.nextInt();
+		
+		Produto produto = new Produto(nome, preco, quantidade);
 		
 		System.out.println();		
 		System.out.println("Dados do produto: " + produto);
 		
 		System.out.println();
 		System.out.println("Entre com o número de produtos a serem adicionados no estoque: ");
-		int quantidade = sc.nextInt();
+		quantidade = sc.nextInt();
 		produto.adicionarProduto(quantidade);
 		
 		System.out.println();
